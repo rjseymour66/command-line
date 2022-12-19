4aee28cae6443b647b1c831b324cc20c16a77af7
rseymour <rjs.github@gmail.com>
2022-12-18 23:55:39 -0500

    updated integration tests pass

--- pScan/cmd/actions_test.go
+++ pScan/cmd/actions_test.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"pScan/scan"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -126,6 +128,10 @@ func TestIntegration(t *testing.T) {
 	expectedOut += fmt.Sprintf("Deleted host: %s\n", delHost)
 	expectedOut += strings.Join(hostsEnd, "\n")
 	expectedOut += fmt.Sprintln()
+	for _, v := range hostsEnd {
+		expectedOut += fmt.Sprintf("%s: Host not found\n", v)
+		expectedOut += fmt.Sprintln()
+	}
 
 	// add -> list -> delete -> list
 	// Add hosts to the list
@@ -143,12 +149,75 @@ func TestIntegration(t *testing.T) {
 		t.Fatalf("Expected no error, got %q\n", err)
 	}
 
-	// List hosts
+	// List hosts after delete
 	if err := listAction(&out, tf, nil); err != nil {
 		t.Fatalf("Expected no error, got %q\n", err)
 	}
 
+	// Scan hosts
+	if err := scanAction(&out, tf, nil); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
 	// Test integration output
+	if out.String() != expectedOut {
+		t.Errorf("Expected output %q, got %q\n", expectedOut, out.String())
+	}
+}
+
+func TestScanAction(t *testing.T) {
+	// Define hosts for scan test
+	hosts := []string{
+		"localhost",
+		"unknownhostoutthere",
+	}
+	// Setup scan test
+	tf, cleanup := setup(t, hosts, true)
+	defer cleanup()
+
+	ports := []int{}
+
+	// Init ports, 1 open, 1 closed
+	for i := 0; i < 2; i++ {
+		ln, err := net.Listen("tcp", net.JoinHostPort("localhost", "0"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer ln.Close()
+
+		_, portStr, err := net.SplitHostPort(ln.Addr().String())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		ports = append(ports, port)
+
+		if i == 1 {
+			ln.Close()
+		}
+	}
+	// Define expected output for scan action
+	expectedOut := fmt.Sprintln("localhost:")
+	expectedOut += fmt.Sprintf("\t%d: open\n", ports[0])
+	expectedOut += fmt.Sprintf("\t%d: closed\n", ports[1])
+	expectedOut += fmt.Sprintln()
+	expectedOut += fmt.Sprintln("unknownhostoutthere: Host not found")
+	expectedOut += fmt.Sprintln()
+
+	// Define var to capture scan output
+	var out bytes.Buffer
+
+	// Execute scan and capture output
+	if err := scanAction(&out, tf, ports); err != nil {
+		t.Fatalf("Expected no error, got %q\n", err)
+	}
+
+	// Test scan output
 	if out.String() != expectedOut {
 		t.Errorf("Expected output %q, got %q\n", expectedOut, out.String())
 	}
